d16783cbc3a975dd2cb11ae692298fbaffebd967
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-21 01:32:14 -0500

    Add project files.

---
+++ Watchtower/Program.cs
@@ -0,0 +1,8 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 9 Watchtower
+// The scenario used a 3x3 grid, but uses relation operators for grid numbers. 
+// The user provides a number and the program provides the direction of the enemy forces
+// Direction is determined based on the number's relationship to 0 for both x and y 
+// The challenge says that you can only use if statements and relational operators.
+
+
