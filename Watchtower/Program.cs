28cdc2fafd0a67de0aa7b33eaea277267d436f64
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-21 01:59:26 -0500

    Created logic for program

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -3,6 +3,41 @@
 // The scenario used a 3x3 grid, but uses relation operators for grid numbers. 
 // The user provides a number and the program provides the direction of the enemy forces
 // Direction is determined based on the number's relationship to 0 for both x and y 
-// The challenge says that you can only use if statements and relational operators.
+// The challenge says to only use "if statements" and relational operators.
 
+Console.WriteLine("Please input a coordinate value for y: ");
+float yCoordinate = Convert.ToSingle(Console.ReadLine());
 
+Console.WriteLine("Please input a coordinate value for x:  ");
+float xCoordinate = Convert.ToSingle(Console.ReadLine());
+
+//Looping through each y then x coordinate
+if (yCoordinate > 0)
+{
+    if (xCoordinate < 0) Console.WriteLine("The enemy is to the northwest!");
+
+    else if (xCoordinate == 0) Console.WriteLine("The enemy is to the north!");
+    
+    else Console.WriteLine("The enemy is to the northeast!");
+    
+}
+else if (yCoordinate == 0)
+{
+    if (xCoordinate < 0) Console.WriteLine("The enemy is to the west!");
+
+    else if (xCoordinate == 0) Console.WriteLine("The enemy on top of us!");
+
+    else Console.WriteLine("The enemy is to the east!");
+
+}
+else // The only choice left is yCoordinate < 0
+{
+    if (xCoordinate < 0) Console.WriteLine("The enemy is to the southwest!");
+
+    else if (xCoordinate == 0) Console.WriteLine("The enemy is to the south!");
+
+    else Console.WriteLine("The enemy is to the southeast!");
+}
+
+Console.WriteLine("Press any key to end");
+Console.ReadKey();
